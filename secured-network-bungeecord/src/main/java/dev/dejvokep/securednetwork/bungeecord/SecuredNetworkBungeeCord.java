c7ac925ef401c565f48a7af2de331c1a99fcccea
dejvokep <davidcubesvkdev@gmail.com>
2022-03-12 20:23:36 +0100

    Shortened debug messages, deleted the updater, added disconnect patches and renamed modules.

---
+++ secured-network-bungeecord/src/main/java/dev/dejvokep/securednetwork/bungeecord/SecuredNetworkBungeeCord.java
@@ -0,0 +1,155 @@
+/*
+ * Copyright 2022 https://dejvokep.dev/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dev.dejvokep.securednetwork.bungeecord;
+
+import dev.dejvokep.boostedyaml.YamlDocument;
+import dev.dejvokep.boostedyaml.dvs.versioning.BasicVersioning;
+import dev.dejvokep.boostedyaml.settings.dumper.DumperSettings;
+import dev.dejvokep.boostedyaml.settings.general.GeneralSettings;
+import dev.dejvokep.boostedyaml.settings.loader.LoaderSettings;
+import dev.dejvokep.boostedyaml.settings.updater.UpdaterSettings;
+import dev.dejvokep.securednetwork.bungeecord.command.PluginCommand;
+import dev.dejvokep.securednetwork.bungeecord.ipwhitelist.AddressWhitelist;
+import dev.dejvokep.securednetwork.bungeecord.listener.LoginListener;
+import dev.dejvokep.securednetwork.bungeecord.message.Messenger;
+import dev.dejvokep.securednetwork.bungeecord.updater.Updater;
+import dev.dejvokep.securednetwork.core.authenticator.Authenticator;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.api.plugin.PluginManager;
+import org.bstats.bungeecord.Metrics;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+
+/**
+ * Main class for the proxy-side of the plugin.
+ */
+public class SecuredNetworkBungeeCord extends Plugin {
+
+    // Plugin
+    private Plugin plugin;
+
+    // Message sender
+    private final Messenger messenger = new Messenger();
+    // Config
+    private YamlDocument config;
+    // Authenticator
+    private Authenticator authenticator;
+    // Address whitelist
+    private AddressWhitelist addressWhitelist;
+    // Login listener
+    private LoginListener listener;
+
+    @Override
+    public void onEnable() {
+        // Set the plugin instance
+        plugin = this;
+        // Thank you message
+        getLogger().info("Thank you for downloading SecuredNetwork!");
+
+        try {
+            // Create the config file
+            config = YamlDocument.create(new File(getDataFolder(), "config.yml"), getResourceAsStream("bungee-config.yml"), GeneralSettings.DEFAULT, LoaderSettings.builder().setAutoUpdate(true).build(), DumperSettings.DEFAULT, UpdaterSettings.builder().setVersioning(new BasicVersioning("config-version")).build());
+        } catch (IOException ex) {
+            getLogger().log(Level.SEVERE, "Failed to initialize the config file! Shutting down...", ex);
+            ProxyServer.getInstance().stop();
+            return;
+        }
+
+        // Initialize
+        authenticator = new Authenticator(config, getLogger());
+
+        // Initialize
+        addressWhitelist = new AddressWhitelist(this);
+
+        // Plugin manager
+        PluginManager pluginManager = ProxyServer.getInstance().getPluginManager();
+        // Register listener
+        pluginManager.registerListener(this, listener = new LoginListener(this));
+        // Register commands
+        pluginManager.registerCommand(this, new PluginCommand(this, "securednetwork"));
+        pluginManager.registerCommand(this, new PluginCommand(this, "sn"));
+
+        // Initialize
+        new Updater(this);
+
+        // If enabled
+        if (config.getBoolean("metrics")) {
+            // Initializing metrics
+            getLogger().info("Initializing metrics.");
+            // Initialize Metrics
+            new Metrics(this, 6479);
+        }
+
+    }
+
+    /**
+     * Returns the plugin instance.
+     *
+     * @return the plugin instance
+     */
+    public Plugin getPlugin() {
+        return plugin;
+    }
+
+    /**
+     * Returns the configuration file representation.
+     *
+     * @return the configuration file.
+     */
+    public YamlDocument getConfiguration() {
+        return config;
+    }
+
+    /**
+     * Returns the authenticator.
+     *
+     * @return the authenticator
+     */
+    public Authenticator getAuthenticator() {
+        return authenticator;
+    }
+
+    /**
+     * Returns the messenger.
+     *
+     * @return the messenger
+     */
+    public Messenger getMessenger() {
+        return messenger;
+    }
+
+    /**
+     * Returns the address whitelist.
+     *
+     * @return the address whitelist
+     */
+    public AddressWhitelist getAddressWhitelist() {
+        return addressWhitelist;
+    }
+
+    /**
+     * Returns the login event listener.
+     *
+     * @return the login event listener
+     */
+    public LoginListener getListener() {
+        return listener;
+    }
+
+}
---
+++ secured-network-bungeecord/src/main/java/dev/dejvokep/securednetwork/bungeecord/updater/Updater.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2022 https://dejvokep.dev/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dev.dejvokep.securednetwork.bungeecord.updater;
+
+import dev.dejvokep.securednetwork.bungeecord.SecuredNetworkBungeeCord;
+import net.md_5.bungee.api.ProxyServer;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Updater class which checks for updates of the plugin by using a website API ({@link #URL}). If enabled, checks for
+ * updates as per the set delay.
+ */
+public class Updater {
+
+    // URL to get latest version
+    public static final String URL = "https://api.spigotmc.org/legacy/update.php?resource=65075";
+
+    /**
+     * Recheck delay in minutes.
+     */
+    private static final long RECHECK_DELAY = 180L;
+
+    /**
+     * Starts the updater (and it's repeating task repeatedly checking for a new update).
+     *
+     * @param plugin the plugin instance
+     */
+    public Updater(@NotNull SecuredNetworkBungeeCord plugin) {
+        // Version
+        String version = plugin.getDescription().getVersion();
+
+        // Schedule
+        ProxyServer.getInstance().getScheduler().schedule(plugin, () -> {
+            // The latest version
+            String latest;
+            try {
+                latest = new BufferedReader(new InputStreamReader(new URL(URL).openStream())).readLine();
+            } catch (IOException ignored) {
+                return;
+            }
+
+            // New version available
+            if (Integer.parseInt(latest.replace(".", "")) > Integer.parseInt(version.replace(".", "")))
+                plugin.getLogger().warning("New version " + latest + " is available! Please update as soon as possible to receive the newest features and important security patches. You are using version " + version + ".");
+        }, 0L, RECHECK_DELAY, TimeUnit.MINUTES);
+    }
+
+}
